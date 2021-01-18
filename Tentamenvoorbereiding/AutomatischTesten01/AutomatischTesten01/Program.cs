6dac906f2088d276420be62cdf69540788978c74
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2021-01-18 20:08:42 +0100

    Recursie voorbeelden met automatische NUnit tests

--- Tentamenvoorbereiding/AutomatischTesten01/AutomatischTesten01/Program.cs
+++ Tentamenvoorbereiding/AutomatischTesten01/AutomatischTesten01/Program.cs
@@ -20,11 +20,44 @@ namespace AutomatischTesten01
             return a + b;
         }
 
-
         [TestCase(ExpectedResult="Hello") ]
         static public string Hello()
         {
             return "Hello";
         }
+
+        [Test]
+        static public void testSom()
+        {
+            Assert.AreEqual(78, CumulatieveSom(12));
+        }
+
+        [TestCase(12, ExpectedResult = 78)]
+        [TestCase(1+2+3+4, ExpectedResult = 10)]
+        static public int CumulatieveSom(int getal)
+        {
+            // Stop conditie
+            if (getal == 0 )
+            {
+                // stop conditie
+                return 0;
+            }
+
+            // wat moet ik met deze situatie doen? in dit geval : onthoudt het huidige getal
+            int nuAanDeBeurt = getal;
+
+            // wat is het resultaat van de rest die overgebleven is?
+            // hamvraag => hoe kun je die 'rest' bepalen? 
+            // in dit geval : de rest wordt bepaald door het huidige getal met 1 te verlagen
+            int resultaatVanDeRest = CumulatieveSom(getal - 1);
+
+            // hoe bepalen we het uiteindelijke resultaat? door de huidige situatie op te tellen
+            // bij het resultaat van de rest.
+            int hetResultaat = nuAanDeBeurt + resultaatVanDeRest;
+
+            // geef dat resultaat terug
+            return hetResultaat;
+        }
+
     }
 }
