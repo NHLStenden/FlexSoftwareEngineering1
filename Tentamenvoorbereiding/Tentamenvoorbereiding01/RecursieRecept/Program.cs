e5f168b0f1a28085f5a7f51dd6bcfe95fd1aedc0
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2021-01-25 20:19:21 +0100

    Meer voorbeelden van recursie

---
+++ Tentamenvoorbereiding/Tentamenvoorbereiding01/RecursieRecept/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace RecursieRecept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(kleinAchterstevoren("Hello World!"));
+        }
+
+        static string klein(string woord)
+        {
+            // stop conditie (stap 1)
+            if (woord == "")
+            {
+                // stap 2: geef een lege string terug zodat een lege string + bestaand resultaat (uit stap 8) = bestaatnd resultaat
+                return "";
+                // mag ook: return woord;
+            }
+            string huidigElement = woord.Substring(0, 1); // stap 3: de eerste letter van het woord
+
+            // stap 4: hier zit dus de eigenlijk werking van deze functie
+            string kleineLetterHuidigElement = huidigElement.ToLower();
+
+            // stap 5: bepaal het restant
+            string restant = woord.Substring(1); // let op: index voor substring begint in 0 (nul)
+
+            // stap 7 (=vang het resultaat op) + 6 (= recursie aanroep)
+            string resultaatVanDeRest = klein(restant);
+
+            // stap 8: +operator = CONCAT = zet achter elkaar in de volgorde van de operands
+            string combinatieResultaat = kleineLetterHuidigElement + resultaatVanDeRest;
+
+            // stap 9
+            return combinatieResultaat;
+        }        
+        
+        static string kleinAchterstevoren(string woord)
+        {
+            // stop conditie (stap 1)
+            if (woord == "")
+            {
+                // stap 2: geef een lege string terug zodat een lege string + bestaand resultaat (uit stap 8) = bestaatnd resultaat
+                return "";
+                // mag ook: return woord;
+            }
+            string huidigElement = woord.Substring(0, 1); // stap 3: de eerste letter van het woord
+
+            // stap 4: hier zit dus de eigenlijk werking van deze functie
+            string kleineLetterHuidigElement = huidigElement.ToLower();
+
+            // stap 5: bepaal het restant
+            string restant = woord.Substring(1); // let op: index voor substring begint in 0 (nul)
+
+            // stap 7 (=vang het resultaat op) + 6 (= recursie aanroep)
+            string resultaatVanDeRest = kleinAchterstevoren(restant);
+
+            // stap 8: +operator = CONCAT = zet achter elkaar in de volgorde van de operands
+            string combinatieResultaat = resultaatVanDeRest + kleineLetterHuidigElement;
+
+            // stap 9
+            return combinatieResultaat;
+        }
+
+        static public string kortEnKlein(string woord)
+        {
+            if (woord == "") return "";
+            return woord.Substring(0, 1).ToLower() + kortEnKlein(woord.Substring(1));
+        }
+        static public string kortEnKleinAchterstevoren(string woord)
+        {
+            if (woord == "") return "";
+            return kortEnKleinAchterstevoren(woord.Substring(1) + woord.Substring(0, 1).ToLower());
+        }
+    }
+}
---
+++ Tentamenvoorbereiding/Tentamenvoorbereiding01/RecursieReceptGetallen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RecursieReceptGetallen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sommeer(4));  // sommeer 4 == 10 == 1+2+3+4
+        }
+
+        static public int sommeer(int getal)
+        {
+            // stop conditie? 
+            if (getal == 0)
+            {
+                return 0;// stap 2
+            }
+
+            int huidigeElement = ; // stap 3
+
+            // stap 4: wat moet ik er mee doen? 
+            int huidigeResultaat = ; // stap 4
+
+            // stap 5
+            int restant = ;
+
+            // step 6 + 7 
+            int resultaatVanDeRest = sommeer(restant);
+
+            // stap 8
+            int combinatieResultaat = ;
+
+            // stap 9
+            return combinatieResultaat;
+
+        }
+    }
+}
