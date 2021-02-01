ad79eabce762bcaf4815a27c8dcead3fff9f51dc
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2021-02-01 21:15:23 +0100

    Extra recursie voorbeelden - recursie met arrays - recursie met getallen - recursie met hulpfunctie

---
+++ Tentamenvoorbereiding/Tentamenvoorbereiding01/RecursieArrays/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace RecursieArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lijstje = { 1, 2, 3, 4, 5, 6, 7, 8, 10, 11,12,13,14,15,16,17,18,19,20 };  // ARRAY (net als string) zijn 'immutable'. ==> je kunt de lengte / aantal elementen in de array niet veranderen. 
+            Console.WriteLine(sommeer(lijstje));
+            Console.WriteLine(sommeerIndexed(lijstje));
+            Console.WriteLine(sommeerVanAchteren(lijstje));
+        }
+
+        static int sommeer(int[] getallen)
+        {
+            if (getallen.Length == 0)
+            {
+                return 0;
+            }
+            int huidigeElement = getallen[0];
+
+            int huidigeResultaat = huidigeElement;
+
+            int[] rest = new int[getallen.Length - 1]; // maak ruimte voor de nieuwe array minus 1 positie, want die hebben we al gehad!
+            // https://stackoverflow.com/questions/733243/how-to-copy-part-of-an-array-to-another-array-in-c
+            Array.Copy(getallen, 1, rest, 0, rest.Length);
+
+            int resultaatVanDeRest = sommeer(rest);
+
+            return huidigeResultaat + resultaatVanDeRest;
+        }
+        static int sommeerVanAchteren(int[] getallen)
+        {
+            if (getallen.Length == 0)
+            {
+                return 0;
+            }
+            int huidigeElement = getallen[getallen.Length - 1];  // pak het laatste element
+
+            int huidigeResultaat = huidigeElement;
+
+            int[] rest = new int[getallen.Length - 1]; // maak ruimte voor de nieuwe array minus 1 positie, want die hebben we al gehad!
+            // https://stackoverflow.com/questions/733243/how-to-copy-part-of-an-array-to-another-array-in-c
+            Array.Copy(getallen, 0, rest, 0, rest.Length);
+
+            int resultaatVanDeRest = sommeer(rest);
+
+            return huidigeResultaat + resultaatVanDeRest;
+        }
+
+
+        // dit is een hulpfunctie om te voorkomen dat de aanroepende partij het getal NUL zelf mee moet geven. 
+        static int sommeerIndexed(int[] getallen)
+        {
+            return sommeerIndexedRecursie(getallen, 0);
+        }
+
+        static int sommeerIndexedRecursie(int[] getallen, int index)
+        {
+            if (index == getallen.Length)
+            {
+                return 0;
+            }
+            return getallen[index] + sommeerIndexedRecursie(getallen, index + 1);
+        }        
+        
+        static int sommeerKwadraatIndexedRecursie(int[] getallen, int index)
+        {
+            // stopconditie : als ik 10 getallen heb, dan is de maximale index = 9 getallen[0] is geldig, getallen[9] is geldig; getallen[10] mag je niet opvragen. je begint bij 0.
+            // dus de index loopt van 0..1..2..3..4..5..6..7..8..9
+            if (index == getallen.Length)
+            {
+                return 0;
+            }
+            return (getallen[index] * getallen[index]) + sommeerKwadraatIndexedRecursie(getallen, index + 1);
+        }
+
+
+    }
+}
--- Tentamenvoorbereiding/Tentamenvoorbereiding01/RecursieReceptGetallen/Program.cs
+++ Tentamenvoorbereiding/Tentamenvoorbereiding01/RecursieReceptGetallen/Program.cs
@@ -7,6 +7,9 @@ namespace RecursieReceptGetallen
         static void Main(string[] args)
         {
             Console.WriteLine(sommeer(4));  // sommeer 4 == 10 == 1+2+3+4
+            Console.WriteLine(sommeerKort(5));  // sommeer 4 == 10 == 1+2+3+4
+            Console.WriteLine(sommeerKwadraten(4));  // sommeer 4 == 1*1 + 2*2 + 3*3 + 4*4 = 1 + 4 + 9 + 16 == 30
+            Console.WriteLine(sommeerKwadraatKort(4));  // sommeer 4 == 1*1 + 2*2 + 3*3 + 4*4 = 1 + 4 + 9 + 16 == 30
         }
 
         static public int sommeer(int getal)
@@ -17,23 +20,72 @@ namespace RecursieReceptGetallen
                 return 0;// stap 2
             }
 
-            int huidigeElement = ; // stap 3
+            int huidigeElement = getal; // stap 3
 
             // stap 4: wat moet ik er mee doen? 
-            int huidigeResultaat = ; // stap 4
+            int huidigeResultaat = huidigeElement; // stap 4
 
             // stap 5
-            int restant = ;
+            int restant = huidigeElement - 1;
 
             // step 6 + 7 
             int resultaatVanDeRest = sommeer(restant);
 
             // stap 8
-            int combinatieResultaat = ;
+            int combinatieResultaat = resultaatVanDeRest + huidigeResultaat;
 
             // stap 9
             return combinatieResultaat;
 
         }
+
+        static public int sommeerKwadraten(int getal)
+        {
+            // stop conditie? 
+            if (getal == 0)
+            {
+                return 0;// stap 2
+            }
+
+            int huidigeElement = getal; // stap 3
+
+            // stap 4: wat moet ik er mee doen?  ==> kwadrateer het huidige element (x => x*x)
+            int huidigeResultaat = huidigeElement * huidigeElement; // stap 4
+
+            // stap 5
+            int restant = huidigeElement - 1;
+
+            // step 6 + 7 
+            int resultaatVanDeRest = sommeerKwadraten(restant);
+
+            // stap 8
+            int combinatieResultaat = resultaatVanDeRest + huidigeResultaat;
+
+            // stap 9
+            return combinatieResultaat;
+        }
+
+
+
+        static public int sommeerKort(int getal)
+        {
+            if (getal == 0 )
+            {
+                return 0;
+            }
+            return getal + sommeerKort(getal - 1);
+        }
+
+
+        static public int sommeerKwadraatKort(int getal)
+        {
+            if (getal == 0 )
+            {
+                return 0;
+            }
+            return (getal * getal) + sommeerKwadraatKort(getal - 1);
+        }
+
+
     }
 }
