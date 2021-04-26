b739e0358f9f3ea0e3d089b11766fe830030f6b1
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2021-04-26 19:32:41 +0200

    Voorbeeld Object Oriented Programming opgenomen

---
+++ Week05/OOP/ElectronischeApparatuur/ElectronischeApparatuur/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+/// <summary>
+/// Dit voorbeeld zou  oorspronkelijk over Electronische Apparatuur gaan (zie .drawio bestand in de bovenliggende folder), maar hierin zaten onvoldoende
+/// aanknopingpunten om met de constructors te werken met vaste / variabele waarden. daarom is een voorbeeld uit de presentatie gekozen waarbij een auto
+/// en vervoersmiddel gebruikt worden. 
+/// </summary>
+namespace ElectronischeApparatuur
+{
+    class Vervoersmiddel
+    {
+        /// <summary>
+        /// standaard zijn alle variabelen in C# gedefinieerd als PRIVATE binnen een klasse. dit kan voor andere talen anders gedefinieerd zijn.
+        /// de variabelen zijn hier voor de duidelijkheid expliciet ingesteld op private.
+        /// </summary>
+        private string kleur;
+        private string fabrikant;
+        private int wielen;
+        private int huidigeSnelheid;
+
+        /// <summary>
+        /// Dit is de constructor die aangeroepen wordt als deze klasse óf een onderliggende klasse gebouwd wordt. 
+        /// </summary>
+        /// <param name="kleur">De kleur van de auto</param>
+        /// <param name="fabrikant">De fabrikant van de auto</param>
+        /// <param name="wielen">Het aantal wielen (er vind geen check plaats op negatieve waarden!)</param>
+        public Vervoersmiddel(string kleur, string fabrikant, int wielen)
+        {
+            this.kleur = kleur;
+            this.fabrikant = fabrikant;
+            this.wielen = wielen;
+            this.zetSnelheid(0);  // in plaats van rechtstreeks de waarde in te stellen gebruiken we een nette methode
+        }
+
+        /// <summary>
+        /// Deze methode stelt de huidige snelheid in op de aangegeven waarde 'v'
+        /// </summary>
+        /// <param name="v">De nieuwe huidige snelheid</param>
+        public void zetSnelheid(int v)
+        {
+            this.huidigeSnelheid = v;
+        }
+    }// clas Vervoersmiddel
+
+
+    /// <summary>
+    /// Deze klasse Auto erft van de klasse Vervoersmiddel. Het opvallendste is dat het aantal wielen vast staat op de waarde 4
+    /// en dus niet meegegeven hoeft te worden bij het aanmaken van een auto.
+    /// 
+    /// Verder voegen we voor een auto een nieuwe eigenschap toe: het aantal deuren. Omdat elke (type) auto een ander aantal deuren kan hebben 
+    /// is het noodzakelijk dat deze bij de constructor meegegeven wordt. 
+    /// </summary>
+    class Auto : Vervoersmiddel
+    {
+        /// <summary>
+        /// een nieuwe variabele om het aantal deuren te registreren. deze dient in de constructor opgegeven te worden.
+        /// </summary>
+        int aantalDeuren;
+
+        /// <summary>
+        /// Deze constructor zorgt voor de initialisatie van een object op basis van de klasse Auto. Eerst wordt de bovenliggende constructor
+        /// (Vervoersmiddel.Vervoersmiddel) aangeroepen door middel van het sleutelwoord 'base' met de parameters kleur, fabrikant en de vaste waarde 4 
+        /// voor het aantal wielen. 
+        /// </summary>
+        /// <param name="aantalDeuren">Het aantal deuren</param>
+        /// <param name="kleur">De kleur van de auto</param>
+        /// <param name="fabrikant">De fabrikant</param>
+        public Auto(int aantalDeuren, string kleur, string fabrikant) : base(kleur, fabrikant, 4)
+        {
+            this.aantalDeuren = aantalDeuren;
+        }
+    }
+
+    /// <summary>
+    /// Deze klasse zorgt voor het beschikbaar komen van de klasse  Renault. Opvallend is hier dat de fabrikant dus altijd Renault is. We kunnen deze
+    /// dus weglaten uit de constructor van DEZE klasse, en geven een vaste waarde mee aan de bovenliggende constructor (Auto.Auto).
+    /// Let op: de aanroep 'base' verwijst dus in dit geval naar de klasse 'Auto' en niet naar 'Vervoersmiddel'!
+    /// </summary>
+    class Renault: Auto
+    {
+        public Renault(int aantalDeuren, string kleur) : base (aantalDeuren, kleur, "Renault")
+        {
+            // doordat we verder geen speciale of nieuwe waarden hoeven aan te passen is dit verder een lege constructor. 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Vervoersmiddel v1 = new Vervoersmiddel("wit", "Opel", 3);
+
+            Auto audi = new Auto(3, "Rood", "Audi");
+
+            // zie het verschil in het aanmaken van een Auto of Renault: de Renault kent geen parameter voor de fabrikant: die staat vast op 'Renault'!
+            Renault clio = new Renault(3, "Geel");
+        }
+
+
+
+    }
+}
--- Week05/OOP/Objecten01/Objecten01/Program.cs
+++ Week05/OOP/Objecten01/Objecten01/Program.cs
@@ -11,7 +11,55 @@ namespace Objecten01
             public string achternaam;
             public int leeftijd;
             public string haarkleur;
+            public void xyz() { Console.WriteLine("help"); }
+            public void print() { Console.WriteLine("Hallo {0} {1}, je bent {2} jaar oud\nKleur haar = {3}", naam, achternaam, leeftijd, haarkleur);  }
         }
+
+        struct WerkendPersoon
+        {
+            public string naam;
+            public string achternaam;
+            public int leeftijd; 
+            public string werkgever;
+            public string functie;
+
+            public void print() {
+                Console.WriteLine("Hallo {0} {1}, je bent {2} jaar oud", naam, achternaam, leeftijd);
+                Console.WriteLine("Werkt bij {0} als {1}", werkgever, functie); 
+            }
+        }
+
+        struct Student
+        {
+            public string naam;
+            public string achternaam;
+            public int leeftijd; 
+            public string studie;
+            public string instelling;
+            public void print() {
+                Console.WriteLine("Hallo {0} {1}, je bent {2} jaar oud", naam, achternaam, leeftijd);
+                Console.WriteLine("Studeert bij {0} opleiding {1}", instelling, studie);  
+            }
+        }
+
+        struct WerkendeStudent
+        {
+            public string naam;
+            public string achternaam;
+            public int leeftijd;
+            public string studie;
+            public string instelling;
+            public string werkgever;
+            public string functie;
+            public bool deeltijd;
+            public void print()
+            {
+                Console.WriteLine("Hallo {0} {1}, je bent {2} jaar oud", naam, achternaam, leeftijd);
+                Console.WriteLine("Werkt bij {0} als {1}", werkgever, functie);
+                Console.WriteLine("Studeert in {2} bij {0} opleiding {1}", instelling, studie, deeltijd ? "deeltijd" : "voltijd");
+            }
+        }
+
         static void Main(string[] args)
         {
 
@@ -26,10 +74,11 @@ namespace Objecten01
 
             // bij de aanroep dus alle variabelen meegeven. onhandig en slecht onderhoudbaar
             PersoonAfdrukken(naam, achternaam, leeftijd, haarkleur);
+            Console.WriteLine("---------------------------------------------------------------------------------------");
 
             // nieuwe manier: maak een struct aan met alle variabelen samengepakt in één variabele
             // je zou die ingepakte variabelen ook 'eigenschappen van het persoon'  kunnen noemen
-            Persoon martin = new Persoon();
+            Persoon martin;
             // vul de waarden van de eigenschappen in. hiervoor gebruiken we de punt-notatie:
             // variabele.eigenschap
             martin.naam = "Martin";
@@ -40,6 +89,47 @@ namespace Objecten01
             // roep de functie aan en geef in één keer makkelijk alle informatie mee.
             // als er nu een eigenschap veranderd hoef ik tenminste niet de parameterlijst aan te passen ('signature')
             PersoonAfdrukkenStructuur(martin);
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+
+            // of nog handiger: met een functie die bij de data hoort
+            martin.print();
+
+            // we gaan nu een persoon met een baan opvoeren
+            WerkendPersoon employee1;
+            employee1.naam = "Martin";
+            employee1.achternaam = "Molema";
+            employee1.leeftijd = 49;
+            employee1.functie = "Docent";
+            employee1.werkgever = "NHL Stenden Hogeschool";
+
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            employee1.print();
+
+            // we gaan nu een student maken
+            Student studentje;
+            studentje.naam = "Martin";
+            studentje.achternaam = "Molema";
+            studentje.leeftijd = 49;
+            studentje.studie = "Master Serious Gaming";
+            studentje.instelling = "NHL Stenden Hogeschool";
+
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            studentje.print();
+
+            
+            // we gaan nu een werkstudent maken
+            WerkendeStudent werkstudent;
+            werkstudent.naam = "Martin";
+            werkstudent.achternaam = "Molema";
+            werkstudent.leeftijd = 49;
+            werkstudent.studie = "Master Serious Gaming";
+            werkstudent.instelling = "NHL Stenden Hogeschool";
+            werkstudent.functie = "Docent";
+            werkstudent.werkgever = "NHL Stenden Hogeschool";
+            werkstudent.deeltijd = true;
+
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            werkstudent.print();
         }
 
         static void PersoonAfdrukken(string naam, string achternaam, int leeftijd, string haarkleur)
@@ -51,6 +141,7 @@ namespace Objecten01
         {
             // we gebruiken hier weer de punt-notatie om de eigenschappen op te halen uit de samengestelde variabele ('struct').
             Console.WriteLine("Hallo {0} {1}, je bent {2} jaar oud\nKleur haar = {3}", p.naam, p.achternaam, p.leeftijd, p.haarkleur);
+            p.xyz();
         }
     }
 }
